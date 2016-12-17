0fcafcfe7178802a9eda9710ec74548f25c12794
eric <eric@ericdeMacBook-Air.local>
2016-12-17 13:39:46 +0800

    cleaned

--- src/pages/user/service.ts
+++ src/pages/user/service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { Http, Response, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
