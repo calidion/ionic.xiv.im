3b964eb0f8a6888bcb07592e058a04e356aca7df
calidion <calidion@gmail.com>
2017-02-10 18:54:40 +0800

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      console.log('ver = 0.1.0');
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
